1fe5f9f0b885b2976c3014589038ec1b2ce74618
Cole Furfaro-Strode <colestrode@gmail.com>
2016-11-30 13:22:46 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const logger = require('@cfs/logger')
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+
+
+app.use(bodyParser.json())
+
+app.post('/consumer', (req, res) => {
+  logger.info('received payload', req.body)
+  res.status(200).send()
+})
+
+app.listen(8000, () => {
+  logger.info('listening on port 8000')
+})
